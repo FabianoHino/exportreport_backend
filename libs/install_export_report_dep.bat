9055f534d2e3b01c6ea317ab27cf153981445f5a
Fabiano Hino <fabiano.hino@corp.carusojrea.com.br>
2020-07-17 09:17:40 -0300

    add dependecies

---
+++ libs/install_export_report_dep.bat
@@ -0,0 +1,8 @@
+@echo off
+
+C:\Python27\ArcGIS10.7\Scripts\pip.exe install Pillow-6.2.2-cp27-cp27m-win_amd64.whl
+C:\Python27\ArcGIS10.7\Scripts\pip.exe install reportlab-3.5.44-cp27-cp27m-win_amd64.whl
+C:\Python27\ArcGIS10.7\Scripts\pip.exe install PyPDF2-1.26.0-cp27-none-any.whl
+
+
+pause
